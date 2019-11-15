2be1eedc9420c73c061be92a76546db5e5af3b3c
James Grantham <james.r.grantham@gmail.com>
2019-11-15 08:22:17 +0000

    Create db functions

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const Users = require('')
 
 router.post('/register', (req, res) => {
   // implement registration
+
 });
 
 router.post('/login', (req, res) => {
